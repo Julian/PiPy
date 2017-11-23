fe14b61af58d403e58e07ab6cab557de77096088
Julian Berman <Julian@GrayVines.com>
2017-11-22 19:30:07 -0500

    DNS.

--- pi/tap.py
+++ pi/tap.py
@@ -1,5 +1,10 @@
-from twisted.application.strports import service
+from twisted.application import internet, strports
+from twisted.application.service import MultiService
 from twisted.python import usage
+import twisted.names.cache
+import twisted.names.client
+import twisted.names.dns
+import twisted.names.server
 
 from pi.core import Pi
 
@@ -12,10 +17,46 @@ class Options(usage.Options):
             None,
             "Path to web CLF (Combined Log Format) log file for access logs.",
         ],
-        ["port", "p", "tcp:8080", "The endpoint to listen on."],
+        ["web", "W", "tcp:8080", "The endpoint to listen on for web."],
+        ["dns", "D", 5333, "The port to listen on for DNS"],
     ]
+
+    optFlags = [
+        ["verbose", "v", "Log verbosely"],
+    ]
+
+    def postOptions(self):
+        try:
+            self["dns"] = int(self["dns"])
+        except ValueError:
+            raise usage.UsageError(
+                "Invalid DNS port: {!r}".format(self["dns"]),
+            )
+
+
+def _makeServices(options):
+    site = Pi().site(logPath=options["access-log"])
+    yield strports.service(description=options["web"], factory=site)
+
+    dnsServer = twisted.names.server.DNSServerFactory(
+        authorities=[],
+        caches=[
+            twisted.names.cache.CacheResolver(verbose=options["verbose"]),
+        ],
+        clients=[
+            twisted.names.client.createResolver(servers=options),
+        ],
+        verbose=options["verbose"],
+    )
+    yield internet.TCPServer(options["dns"], dnsServer)
+    yield internet.UDPServer(
+        options["dns"],
+        twisted.names.dns.DNSDatagramProtocol(dnsServer),
+    )
 
 
 def makeService(options):
-    site = Pi().site(logPath=options["access-log"])
-    return service(options["port"], site)
+    service = MultiService()
+    for each in _makeServices(options=options):
+        each.setServiceParent(service)
+    return service
